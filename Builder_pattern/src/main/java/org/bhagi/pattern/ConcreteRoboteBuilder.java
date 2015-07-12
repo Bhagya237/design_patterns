a64f29ed770621c39d10ee754b32f60bbd96f1f0
Charith Bhagya <charith@rcapl.com>
2015-07-12 09:05:00 +0530

    Moving project to maven

--- Builder_pattern/src/main/java/org/bhagi/pattern/ConcreteRoboteBuilder.java
+++ Builder_pattern/src/main/java/org/bhagi/pattern/ConcreteRoboteBuilder.java
@@ -8,31 +8,27 @@ public class ConcreteRoboteBuilder implements AbstractRobotBuilder {
 		this.robot=new ProductRobot();
 	}
 	
-	@Override
+	
 	public void buildRobotHead() {
 		robot.setRobotHead("Head");
 		
 	}
 
-	@Override
 	public void buildRobotTorso() {
 		robot.setRobotTorso("Toto");
 		
 	}
 
-	@Override
 	public void buildRobotArms() {
 		robot.setRobotArms("Arms");
 		
 	}
 
-	@Override
 	public void buildRobotLegs() {
 		robot.setRobotLegs("legs");
 		
 	}
 
-	@Override
 	public ProductRobot getRobot() {
 		// TODO Auto-generated method stub
 		return this.robot;
